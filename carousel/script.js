1974f0c4273c07aebce5e6dd6bb1b7fd4ceaeeb2
wky <wky_0615@163.com>
2017-11-04 23:35:36 +0800

    commit

--- carousel/script.js
+++ carousel/script.js
@@ -11,12 +11,31 @@ Carousel.prototype = {
     this.$forward = this.$ct.find('#icon-left')
     this.$next = this.$ct.find('#icon-right')
     this.$btn = this.$ct.find('.btn')
+    
+    this.$carousel.append(this.$li.first().clone())
+    this.$carousel.prepend(this.$li.last().clone())
+    
+    this.$carousel.width((this.$li.length+2)*this.$li.width())
   },
   event: function(){
+    this.$forward.on('cick', function(){
+      console.log('forward')
+    })
+    this.$next.on('cick', function(){
+      console.log('next')
+    })
+    this.$btn.on('cick', function(){
+      console.log($(this).index)
+    })
+  },
+  playForward: function(){
+    
+  },
+  platNext: function(){
     
   }
 }
 
 
 new Carousel($('.viewport').eq(0))
-nwe Carousel($('.viweport').eq(1))
+new Carousel($('.viewport').eq(1))
